082df3c87e98549c8924f1462bcf2daa065636ba
Tom <fincht96@gmail.com>
2020-07-18 23:59:45 +0100

    Revised design of login and register webpages

--- passport.config.js
+++ passport.config.js
@@ -5,14 +5,16 @@ function initialize(passport, getUserByEmail, getUserById) {
   const authenticateUser = async (email, password, done) => {
     const user = getUserByEmail(email);
     if (user == null) {
-      return done(null, false, { message: "No user with that email" });
+      return done(null, false, {
+        message: "No user with that email, try again",
+      });
     }
 
     try {
       if (await bcrypt.compare(password, user.password)) {
         return done(null, user);
       } else {
-        return done(null, false, { message: "Password incorrect" });
+        return done(null, false, { message: "Password incorrect, try again" });
       }
     } catch (e) {
       return done(e);
