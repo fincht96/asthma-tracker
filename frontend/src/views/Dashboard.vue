4875b150a6bda4f134cc6fb7349570c9b33a4142
Thomas Finch <fincht96@gmail.com>
2020-09-01 19:58:24 +0100

    Created add new reading modal

--- frontend/src/views/Dashboard.vue
+++ frontend/src/views/Dashboard.vue
@@ -1,6 +1,65 @@
 <template>
   <div id="dashboard">
-    <div class="action-icon">+</div>
+    <div class="modal" v-bind:class="{ 'is-active' : showModal }">
+      <div class="modal-background" v-on:click="showModal = false"></div>
+      <div class="modal-content">
+        <div
+          style="width:80%; max-width:600px; background:white; margin:auto auto; padding: 20px; text-align:left;"
+        >
+          <h4 class="subtitle is-4">Add New Reading</h4>
+
+          <br />
+
+          <div class="field">
+            <label class="label" style="font-size:14px;">When</label>
+            <div class="control">
+              <input style="font-size:14px;" class="input" type="text" />
+            </div>
+          </div>
+
+          <div style="height:10px;" />
+
+          <div class="field">
+            <label class="label" style="font-size:14px;">Peak Flow (L/min)</label>
+            <div class="control">
+              <input style="font-size:14px;" class="input" type="number" />
+            </div>
+          </div>
+
+          <div style="height:10px;" />
+
+          <div class="field">
+            <label class="label" style="font-size:14px; width:100%;">Medication</label>
+            <div class="control">
+              <div class="select" style="font-size:14px; width:100%;">
+                <select style="font-size:14px; width:100%;">
+                  <option>None</option>
+                  <option>Pre-Med</option>
+                  <option>Post-Med</option>
+                </select>
+              </div>
+            </div>
+          </div>
+
+          <div style="height:10px;" />
+
+          <div class="field">
+            <label class="label" style="font-size:14px;">Comment</label>
+            <div class="control">
+              <textarea class="textarea" style="font-size:14px;"></textarea>
+            </div>
+          </div>
+
+          <br>
+
+
+          <button class="button is-primary">Add</button>
+        </div>
+      </div>
+      <button class="modal-close is-large" aria-label="close" v-on:click="showModal = false"></button>
+    </div>
+
+    <div class="action-icon" v-on:click="showModal = true">+</div>
 
     <div id="menu">
       <div class="flex-container">
@@ -34,7 +93,7 @@
         <thead>
           <tr>
             <th>When</th>
-            <th >Peak Flow (L/min)</th>
+            <th>Peak Flow (L/min)</th>
             <th>Medication</th>
             <th style="width:55%">Comment</th>
           </tr>
@@ -156,7 +215,8 @@ export default {
 
   data: function() {
     return {
-      dropdownOpen: true
+      dropdownOpen: true,
+      showModal: false
     };
   }
 };
